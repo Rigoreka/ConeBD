4f330299f6bc7ed9d3b814aefb64f509d42fb5c8
Rigoreka <rodrigojoaolopezmarenco2@gmail.com>
2023-10-10 21:56:06 -0600

    Agregar archivos de proyecto.

---
+++ ConexionBD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ConeBD
+{
+    public class ConexionBD : DbContext
+    {
+        public ConexionBD() : base("name=MiConexionBD")
+        {
+
+        }
+
+        public DbSet<Proyectos> Proyecto { get; set; }
+
+
+    }
+
+    public class Proyectos
+    {
+        [Key]
+        public int ProyectoId { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFinal { get; set; }
+        public string Estado { get; set; }
+    }
+
+
+}
---
+++ Global.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConeBD
+{
+    public class Global
+    {
+        public static List<Proyectos> ListarProyectos()
+        {
+            using (var dbContext = new ConexionBD())
+            {
+                return dbContext.Proyecto.ToList();
+            }
+        }
+
+
+
+        public static bool GuardarProyecto(string nombre, DateTime fechaInicio, DateTime fechaFinal, string estado)
+        {
+            try
+            {
+                using (var dbContext = new ConexionBD())
+                {
+                    var proyecto = new Proyectos
+                    {
+                        Nombre = nombre,
+                        FechaInicio = fechaInicio,
+                        FechaFinal = fechaFinal,
+                        Estado = estado
+                    };
+
+                    dbContext.Proyecto.Add(proyecto);
+                    dbContext.SaveChanges();
+                    return true; // Operación de guardado exitosa
+                }
+            }
+            catch (Exception ex)
+            {
+                // Registra el mensaje de error
+                Console.WriteLine("Error al guardar el proyecto: " + ex.Message);
+                return false; // Operación de guardado fallida
+            }
+
+        }
+    }
+}
